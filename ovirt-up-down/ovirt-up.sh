80652f5b96b81a6ba707581784c92416573d292e
Aren <7918248+funixz@users.noreply.github.com>
2020-08-15 22:27:36 -0400

    Update ovirt-up.sh

--- ovirt-up-down/ovirt-up.sh
+++ ovirt-up-down/ovirt-up.sh
@@ -17,7 +17,7 @@ ansible-playbook -i virt-hosts.inv start-broker-agent.yml
 echo -e '\nAnsible will turn off global maintenance mode.. \n'
 #4) Move hosted-engine out of global maintenance by running the command
 # hosted-engine --set-maintenance --mode=none
-ansible-playbook -i virt-hosts.inv global-maint.yml
+ansible-playbook -i virt-hosts.inv global-maint.yml -e "globalornone=none"
 
 #5) give some time for the HE to come up. check for 'hosted-engine --vm-status' to see if HE vm is up.
 echo -e '\nGive some time for the HE to come up. \nCheck for 'hosted-engine --vm-status' to see if HE vm is up.\n'
